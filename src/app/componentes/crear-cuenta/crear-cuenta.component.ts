b22ef83fa9fd6cacb1b28988aa1386da3927f2e6
martin011001 <martingagliardi2001@gmail.com>
2023-04-04 22:42:41 -0300

    -Fix: header y divs, Refactor: Header descripcion

--- src/app/componentes/crear-cuenta/crear-cuenta.component.ts
+++ src/app/componentes/crear-cuenta/crear-cuenta.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
-import { Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-cuenta',
@@ -9,28 +9,33 @@ import { Validators} from '@angular/forms';
 })
 export class CrearCuentaComponent {
 
-  @Input() estilo:String = "";
+  @Input() estilo: String = "";
   @Output() salir = new EventEmitter;
 
   form: FormGroup;
 
-  constructor(private formBuilder:FormBuilder) {
-    
+  constructor(private formBuilder: FormBuilder) {
+
     this.form = this.formBuilder.group({
-      mail:['', [Validators.required, Validators.email, Validators.maxLength(50)]],
-      password:['',[Validators.required, Validators.minLength(8)]]
+      mail: ['', [Validators.required, Validators.email, Validators.maxLength(50)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      nombre: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      apellido: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      asunto: ['', [Validators.required, Validators.maxLength(100)]],
+      mensaje: ['', [Validators.required, Validators.maxLength(3000)]]
+
     });
   }
 
-  get password(){
+  get password() {
     return this.form.get("password");
   }
- 
-  get mail(){
-   return this.form.get("mail");
+
+  get mail() {
+    return this.form.get("mail");
   }
 
-  get passwordValid(){
+  get passwordValid() {
     return this.password?.touched && !this.password?.valid;
   }
 
@@ -38,23 +43,44 @@ export class CrearCuentaComponent {
     return false
   }
 
+  get nombre(){
+    return this.form.get("nombre");
+  }
+  get nombreRequired(){
+    return false;
+  }
 
-  onEnviar(event: Event){
+  get apellido(){
+    return this.form.get("apellido");
+  }
+  get apellidoRequired(){
+    return false;
+  }
+
+  get asunto(){
+    return this.form.get("asunto");
+  }
+  get mensaje(){
+    return this.form.get("mensaje");
+  }
+
+
+  onEnviar(event: Event) {
     // Detenemos la propagación o ejecución del compotamiento submit de un form
-    event.preventDefault; 
- 
-    if (this.form.valid){
+    event.preventDefault;
+
+    if (this.form.valid) {
       // Llamamos a nuestro servicio para enviar los datos al servidor
       // También podríamos ejecutar alguna lógica extra
       alert("Todo salio bien ¡Enviar formuario!")
-    }else{
+    } else {
       // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
-      this.form.markAllAsTouched(); 
+      this.form.markAllAsTouched();
     }
- 
+
   }
 
-  salirDeLogin(){
+  salirDeLogin() {
     this.salir.emit();
   }
 
