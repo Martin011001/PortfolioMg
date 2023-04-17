5fdd584ef9b35d62117c72c66303c7107bfeb172
Your Namecls <you@example.com>
2023-04-16 23:54:33 -0300

    -Refactor: Conocimiento, modularizado

--- src/app/componentes/contenido/conocimiento/item-conocimiento/item-conocimiento.component.ts
+++ src/app/componentes/contenido/conocimiento/item-conocimiento/item-conocimiento.component.ts
@@ -6,10 +6,27 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./item-conocimiento.component.css']
 })
 export class ItemConocimientoComponent {
+  
+  color: String = "";
 
   @Input() itemConocimiento:any = "";
 
   constructor(){
     
   }
+
+  colorProgreso(): String{
+    if(this.itemConocimiento.progreso <= 25 ){
+      this.color = "progress-bar bg-danger" 
+    }else if(this.itemConocimiento.progreso > 25 && this.itemConocimiento.progreso <= 50){
+      this.color = "progress-bar bg-info text-dark"
+    }else if(this.itemConocimiento.progreso > 50 && this.itemConocimiento.progreso <= 75){
+      this.color = "progress-bar bg-warning text-dark"
+    }else{
+      this.color = "progress-bar bg-success"
+    }
+
+    return  this.color;
+  }
+
 }
